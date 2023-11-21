78dd32171942ced2e2114b5ce9556d6a6600830d
Gianna <gbauzil02@gmail.com>
2023-11-21 14:13:43 -0500

    removing jwt commands

--- backend/api/getClientProfilesController.py
+++ backend/api/getClientProfilesController.py
@@ -1,7 +1,6 @@
 from app import api, app
 from flask_restx import Resource, fields
 from service.getClientProfilesService import getClientProfiles
-from flask_jwt_extended import jwt_required
 
 genInfo_model=api.model(
     "genInfo",
@@ -30,7 +29,6 @@ genInfo_model=api.model(
 @api.route('/clients/profiles')
 class ClientsRescource(Resource):
     @api.marshal_list_with(genInfo_model)
-    @jwt_required()
     def get(self):
         """Get Client profiles"""
         info = getClientProfiles()
